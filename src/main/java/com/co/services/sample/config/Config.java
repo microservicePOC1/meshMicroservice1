0b75823b8fcad5d5733463c63477dde766ddc66e
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-10-17 13:50:54 -0400

    Update Config.java

--- src/main/java/com/co/services/sample/config/Config.java
+++ src/main/java/com/co/services/sample/config/Config.java
@@ -8,6 +8,14 @@ import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+//kevault imports
+import com.azure.core.util.polling.SyncPoller;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.DeletedSecret;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 
 import javax.sql.DataSource;
 
@@ -25,7 +33,47 @@ public class Config {
     @Primary
     @Bean("mainDataSource")
     public DataSource mainDataSource() {
-         
+    	//start of keyvault
+    	String keyVaultName = System.getenv("KEY_VAULT_NAME");
+        String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+
+        System.out.printf("key vault name = %s and key vault URI = %s \n", keyVaultName, keyVaultUri);
+
+        SecretClient secretClient = new SecretClientBuilder()
+            .vaultUrl(keyVaultUri)
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildClient();
+
+        Console con = System.console();  
+
+        String secretName = "mySecret";
+
+        System.out.println("Please provide the value of your secret > ");
+        
+        String secretValue = con.readLine();
+
+        System.out.print("Creating a secret in " + keyVaultName + " called '" + secretName + "' with value '" + secretValue + "` ... ");
+
+        secretClient.setSecret(new KeyVaultSecret(secretName, secretValue));
+
+        System.out.println("done.");
+        System.out.println("Forgetting your secret.");
+        
+        secretValue = "";
+        System.out.println("Your secret's value is '" + secretValue + "'.");
+
+        System.out.println("Retrieving your secret from " + keyVaultName + ".");
+
+        KeyVaultSecret retrievedSecret = secretClient.getSecret(secretName);
+
+        System.out.println("Your secret's value is '" + retrievedSecret.getValue() + "'.");
+        System.out.print("Deleting your secret from " + keyVaultName + " ... ");
+
+        SyncPoller<DeletedSecret, Void> deletionPoller = secretClient.beginDeleteSecret(secretName);
+        deletionPoller.waitForCompletion();
+
+        System.out.println("done.");
+         //end of keyvault
         String serverName = environment.getProperty("environment.servername");
         String port = environment.getProperty("environment.dbport");
         String databaseName = environment.getProperty("environment.dbname");
