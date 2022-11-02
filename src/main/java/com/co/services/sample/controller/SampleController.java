0b3cd81543d178a2a2e34cd74b6969430b6d69ac
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-11-02 16:50:36 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.core.env.Environment;
-import com.service.models.*;
+import com.co.services.sample.models.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
